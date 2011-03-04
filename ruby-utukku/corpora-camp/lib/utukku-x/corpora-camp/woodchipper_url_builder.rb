7b5a90ee4669ac169259a63c2c85c401f474b3b6
James Smith <jgsmith@gmail.com>
2011-03-04 11:38:59 -0500

    pure-ruby version of url builder

--- ruby-utukku/corpora-camp/lib/utukku-x/corpora-camp/woodchipper_url_builder.rb
+++ ruby-utukku/corpora-camp/lib/utukku-x/corpora-camp/woodchipper_url_builder.rb
@@ -1,27 +1,76 @@
-require "java"
-# TODO: fix path to jar file
-require "target/woodchipper-0.0.1-jar-with-dependencies.jar"
-
 class UtukkuX::CorporaCamp::WoodchipperURLBuilder < Utukku::Engine::TagLib
 
-  @@builder = org.projectbamboo.corporacamp.metadata.WoodchipperURLBuilder.new()
+  namespace 'http://www.example.com/corpora-camp/ns/woodchipper/url-builder'
 
-  NS = 'http://www.example.com/corpora-camp/ns/woodchipper/url-builder'
-  namespace NS
+  function 'build-text-url' do |ctx, args|
+    a = args[0]
+    begin
+      args[0] = args[0].flatten.first
+      if args[0].value.nil?
+        args[0] = args[0].children
+      end
+    rescue
+      args[0] = a
+    end
+    collection = args[0].flatten.first.value
 
-  function 'build-text-url', do |ctx, args|
-    collection = args[0].to_s
-    ids = args[1].to_a
-    Utukku::Engine::MapIterator(ids) do |id|
-      @@builder.buildTextURL(collection, id)
+    a = args[1]
+    begin
+      args[1] = args[1].flatten.first
+      if args[1].value.nil?
+        args[1] = args[1].children
+      end
+    rescue
+      args[1] = a
+    end
+    ids = args[1].flatten.first.value
+
+    case collection
+      when 'hathi'
+        return "http://hdl.handle.net/2027/#{ids['textid']}"
+      when 'perseus'
+      when 'eebo'
+        if ids.include?('marc')
+          return "http://gateway.proquest.com/openurl?ctx_ver=Z39.88-2003&res_id=xri:eebo&rft_id=xri:eebo:citation:#{ids['marc']}"
+        end
+      when 'ecco'
+      when 'evans'
     end
   end
 
-  function 'build-chunk-url', do |ctx, args|
-    collection = args[0].to_s
-    ids = args[1].to_a
-    Utukku::Engine::MapIterator(ids) do |id|
-      @@builder.buildChunkURL(collection, id)
+  function 'build-chunk-url' do |ctx, args|
+    a = args[0]
+    begin
+      args[0] = args[0].flatten.first
+      if args[0].value.nil?
+        args[0] = args[0].children
+      end
+    rescue
+      args[0] = a
+    end
+    collection = args[0].flatten.first.value
+
+    a = args[1]
+    begin
+      args[1] = args[1].flatten.first
+      if args[1].value.nil?
+        args[1] = args[1].children
+      end
+    rescue
+      args[1] = a
+    end
+    ids = args[1].flatten.first.value
+
+    case collection
+      when 'hathi'
+        return "http://babel.hathitrust.org/cgi/pt?seq=146&view=image&size=100&id=#{ids["textid"]}&u=1&num=#{ids["chunkid"]}"
+      when 'perseus'
+      when 'eebo'
+        if ids.include?('vid')
+          return "http://gateway.proquest.com/openurl?ctx_ver=Z39.88-2003&res_id=xri:eebo&rft_id=xri:eebo:image:#{ids["vid"]}:#{ids["chunkid"]}"
+        end
+      when 'ecco'
+      when 'evans'
     end
   end
 end
